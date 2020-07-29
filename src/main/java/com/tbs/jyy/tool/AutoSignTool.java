6efaaa1dbbc2a5f90e36790e775bdad652c8641d
蒋勇焱 <573629687@qq.com>
2020-07-29 14:03:08 +0800

    项目创建

---
+++ src/main/java/com/tbs/jyy/tool/AutoSignTool.java
@@ -0,0 +1,12 @@
+package com.tbs.jyy.tool;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tbs-jyy
+ * @description 用来做自动签到脚本
+ * @date 2020/7/29
+ */
+@Component
+public class AutoSignTool {
+}
